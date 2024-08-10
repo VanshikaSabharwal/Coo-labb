0131f55449e8b46b696a086e5b50862163c1e84a
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2024-08-10 13:26:18 +0530

    re added .next

--- src/app/api/acceptFriend/route.ts
+++ src/app/api/acceptFriend/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   const token = req.body;
   if (token) {
     return res.status(200).json({ message: "Friend request accepted." });
